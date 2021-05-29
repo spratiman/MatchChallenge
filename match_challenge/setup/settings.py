17a6af9f9f257c1b34ec5d30d8c9ae1402b49697
Pratiman Shahi <spratiman@yahoo.com>
2021-05-29 00:08:23 -0400

    Add dummy data for Notice

--- match_challenge/setup/settings.py
+++ match_challenge/setup/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from django.conf.global_settings import FIXTURE_DIRS
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -78,6 +80,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+FIXTURE_DIRS = ['fixtures']
 
 
 # Internationalization
