a72ed32f5c8341ba492b805acb20243193c1ce15
Pratiman Shahi <spratiman@yahoo.com>
2021-05-28 22:23:13 -0400

    Add serializers for Notice, Record and Match models

---
+++ match_challenge/app/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from models import Notice, Record, Match
+
+
+class NoticeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notice
+        fields = "__all__"
+
+
+class RecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Record
+        fields = "__all__"
+
+
+class MatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Match
+        fields = "__all__"
