c1699a78fc648356928d7f8fe811aefb5beb4374
Pratiman Shahi <spratiman@yahoo.com>
2021-05-28 23:48:45 -0400

    Fix records url

--- match_challenge/api/v1/urls.py
+++ match_challenge/api/v1/urls.py
@@ -6,7 +6,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 
 router.register("notices", NoticeViewSet)
-router.register("record", RecordViewSet)
+router.register("records", RecordViewSet)
 router.register("matches", MatchViewSet)
 
 
